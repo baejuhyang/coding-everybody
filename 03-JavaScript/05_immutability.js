b18babe94d53409d890a3dddeafe27f7a9b084f1
JH <juhyang9897@gmail.com>
2025-04-09 10:57:45 +0900

    Object.freeze()

--- 03-JavaScript/05_immutability.js
+++ 03-JavaScript/05_immutability.js
@@ -66,6 +66,47 @@
 // console.log(score); // [ 1, 2, 3, 4 ]
 
 // 원본을 mutable하게 처리할 수 있는 API // 원본을 안바꾸는 방법
-let score = [1, 2, 3];
-let score2 = score.concat(4); // 원본은 보전하지만 성능이 느려짐
-console.log(score, score2); // [ 1, 2, 3 ] [ 1, 2, 3, 4 ]
+// let score = [1, 2, 3];
+// let score2 = score.concat(4); // 원본은 보전하지만 성능이 느려짐
+// console.log(score, score2); // [ 1, 2, 3 ] [ 1, 2, 3, 4 ]
+
+// Object.freeze
+// 객체를 얼려 누구도 그 객체를 수정하지 못하게 함.
+// let o1 = { name: 'kim', score: [1, 2] };
+// Object.freeze(o1);
+// o1.name = 'lee'; // 수정되지 않음
+// o1.city = 'seoul'; // 추가되지 않음
+// console.log(o1); // { name: 'kim', score: [ 1, 2 ] } // 원본 보존
+
+// unfreeze하는 명령은 제공하지 않음. // freeze를 풀고 싶다면, 복제해서 사용
+
+// 객체의 property의 값이 객체라면 걔를 규제할 수는 없음
+// o1.score.push(3);
+// console.log(o1); // { name: 'kim', score: [ 1, 2, 3 ] }
+// o1.score에는 값이 저장되어 있지 않고 reference만 저장되어 있기 때문
+// 안의 객체도 얼려야 함.
+// Object.freeze(o1.score);
+// o1.score.push(4);
+// console.log(o1); // 에러
+
+// Object.free()와 const의 차이
+// const o1 = { name: 'kim' };
+// const o2 = { name: 'lee' };
+// // o1 = o2; // 에러 // o1이라는 변수가 가르키는 대상이 될 값을 바꾸지 못하게 함. 참조가 바뀌지 않게 함.
+// o1.name = 'park'; // 값은 바뀜
+// console.log(o1); // { name: 'park' }
+
+// Object.freeze(o1);
+// o1.name = 'park'; // 값이 안바뀜
+// console.log(o1); // { name: 'kim' }
+
+let o1 = { name: 'kim' };
+let o2 = { name: 'lee' };
+Object.freeze(o1); // 값을 바꾸지 못하게 하는 것은 Object.freeze();
+// o1.name = 'lee'; // 값이 안바뀜
+// console.log(o1); // { name: 'kim' }
+
+o1 = o2;
+console.log(o1); // { name: 'lee' } // 참조가 바뀜
+o1.name = 'park';
+console.log(o1); // { name: 'park' } // 여기서 참조하는 것은 freeze하지 않았기에 값이 바뀜
