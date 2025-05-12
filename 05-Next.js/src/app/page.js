0867f3e993eb96209a55107d2605cceb80f3dcdd
JH <juhyang9897@gmail.com>
2025-05-12 20:54:09 +0900

    Next.js 설치

---
+++ 05-Next.js/src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+export default function Home() {
+    return (
+        <>
+            <main>
+                <h2>Welcome</h2>
+                Hello, WEB!
+            </main>
+        </>
+    );
+}
