8640705c2f5748685048cfa034726097653899a0
JH <juhyang9897@gmail.com>
2025-06-01 23:02:30 +0900

    라우팅

---
+++ 05-Next.js/src/app/read/[id]/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Read = (props) => {
+    return (
+        <div>
+            <h2>Read</h2>
+            parameter : {props.params.id}
+        </div>
+    );
+};
+
+export default Read;
