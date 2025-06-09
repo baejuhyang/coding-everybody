2d845f009baa35bef6881c562bbc51448cf77fc6
JH <juhyang9897@gmail.com>
2025-06-09 22:45:31 +0900

    Delete 삭제

--- 05-Next.js/src/app/Control.js
+++ 05-Next.js/src/app/Control.js
@@ -1,11 +1,12 @@
 'use client'; // client component로
 import Link from 'next/link';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 
 export default function Control() {
     const params = useParams();
     // console.log(params); // {id: '1'} // 해당하는 글의 id값을 가져옴
     const id = params.id;
+    const router = useRouter();
 
     return (
         <ul>
@@ -18,7 +19,23 @@ export default function Control() {
                         <Link href={`/update/${id}`}>Update</Link>
                     </li>
                     <li>
-                        <input type="button" value="Delete" />
+                        <input
+                            type="button"
+                            value="Delete"
+                            onClick={() => {
+                                const options = { method: 'DELETE' };
+                                fetch(
+                                    `http://localhost:9999/topics/${id}`,
+                                    options
+                                )
+                                    .then((resp) => resp.json())
+                                    .then((result) => {
+                                        // 삭제 시 홈으로 리디렉션
+                                        router.push(`/`);
+                                        router.refresh();
+                                    });
+                            }}
+                        />
                     </li>
                 </>
             ) : null}
